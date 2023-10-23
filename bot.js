a2938e7d3c6aa8295d0ec8c722e9ec05c036c5ee
Mathieu <mathieu@fellicht.nl>
2023-10-23 09:49:22 +0200

    Bot for az

--- bot.js
+++ bot.js
@@ -17,8 +17,36 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-console.log(user);
+bot.on('message', function(user, userID, channelID, message, event) {
+
+    let options = [];
+    options.push("you'll have to farm");
+    options.push("I plan on farming");
+    options.push("That's a tough farm");
+    options.push("That's a long farm");
+    options.push("New character");
+    options.push("New toon");
+    options.push("Shiny");
+    options.push("Shinies");
+
+    for (i = 0; i < options.length; i++) {
+        if (message.toLowerCase().includes(options[i].toLowerCase())) {
+
+            bot.sendMessage({
+                to: channelID,
+                message: "I'm interested in that"
+            });
+            return;
+        }
+    }
+    return;
+if (["I'm planning on farming", "You'll have to farm"].includes(message)) {
+    bot.sendMessage({
+        to: channelID,
+        message: "I'm interested in that"
+    });
+    return;
+}
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 5) == '/roll') {
